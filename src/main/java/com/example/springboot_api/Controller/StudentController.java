343f412edbec8c91e7ec5b283c25a4c41ccb95c2
maihuytech@gmail.com <maihuytech@gmail.com>
2021-09-17 20:03:52 +0700

    Commit DTO

--- src/main/java/com/example/springboot_api/Controller/StudentController.java
+++ src/main/java/com/example/springboot_api/Controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.springboot_api.Controller;
 
+import com.example.springboot_api.DTO.StudentDTO;
 import com.example.springboot_api.Entity.Student;
 import com.example.springboot_api.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,27 +17,46 @@ import java.util.List;
 public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
+
+    @GetMapping("/students/DTO")
+    public List<StudentDTO> getDTOById() {
+        List<Student> students = studentRepository.findAll();
+        List<StudentDTO> studentDTOS = new ArrayList<>();
+        for(Student student: students){
+            StudentDTO studentDTO = new StudentDTO();
+            studentDTO.setNameStudent(student.getNameStudent());
+            studentDTO.setId(student.getId());
+              studentDTOS.add(studentDTO);
+        }
+        return studentDTOS;
+    }
+
     @GetMapping("/students/{id}")
-    public Student getById(@PathVariable long id){
+    public Student getById(@PathVariable long id) {
         return studentRepository.findById(id).get();
     }
+
     @GetMapping("/students")
-    public List<Student> getAll(){
+    public List<Student> getAll() {
         return studentRepository.findAll();
     }
+
     @PostMapping("/students")
-    public String save(@Valid @RequestBody Student student){
-         studentRepository.save(student);
-        return "save success"+ LocalDateTime.now();
-    }
-    @PutMapping("/students")
-    public String update(@Valid @RequestBody Student student){
+    public String save(@Valid @RequestBody Student student) {
         studentRepository.save(student);
-        return "Put success"+ LocalDateTime.now();
+        return "save success" + LocalDateTime.now();
     }
+
+    @PutMapping("/students")
+    public String update(@Valid @RequestBody Student student) {
+        studentRepository.save(student);
+        return "Put success" + LocalDateTime.now();
+    }
+
     @DeleteMapping("/students/{id}")
-    public String Delete(@PathVariable long id){
+    public String Delete(@PathVariable long id) {
         studentRepository.deleteById(id);
-        return "Put success"+ LocalDateTime.now();
+        return "Put success" + LocalDateTime.now();
     }
+
 }
---
+++ src/main/java/com/example/springboot_api/DTO/StudentDTO.java
@@ -0,0 +1,20 @@
+package com.example.springboot_api.DTO;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.*;
+
+/**
+ * @author MPHuy on 17/09/2021
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class StudentDTO {
+    private int id;
+    private String nameStudent;
+
+}
