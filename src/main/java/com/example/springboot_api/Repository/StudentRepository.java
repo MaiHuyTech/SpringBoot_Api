201e94f778603279053a0720dc29cc4a638c19fb
maihuytech@gmail.com <maihuytech@gmail.com>
2021-09-17 09:09:38 +0700

    commit Validation

--- src/main/java/com/example/springboot_api/Repository/StudentRepository.java
+++ src/main/java/com/example/springboot_api/Repository/StudentRepository.java
@@ -3,6 +3,6 @@ package com.example.springboot_api.Repository;
 import com.example.springboot_api.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student,Long>     {
 
 }
